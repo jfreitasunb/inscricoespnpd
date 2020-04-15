7339a4d0a940f0d4af3cee4d873c648d5856829d
José Antônio <jfreitas@mat.unb.br>
2020-04-15 20:00:03 -0300

    Dados para template.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,27 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use View;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public $periodo_inscricao;
+
+    public $texto_inscricao_pos;
+
+    public function __construct()
+    {
+        // $inscricao_pos = new ConfiguraInscricaoPos();
+
+        // $periodo_inscricao = $inscricao_pos->retorna_periodo_inscricao();
+
+        // $texto_inscricao_pos = $inscricao_pos->define_texto_inscricao();
+        // 
+        
+        $periodo_inscricao = "01/04/2020 à 31/12/2020";
+
+        View::share ( 'periodo_inscricao', $periodo_inscricao );
+    }
 }
