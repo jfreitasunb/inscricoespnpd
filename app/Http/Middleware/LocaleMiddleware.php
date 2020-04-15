3537baf9c15928b779e536ef574c43ea6a8a695e
José Antônio <jfreitas@mat.unb.br>
2020-04-15 18:53:51 -0300

    Middleware para definição de idioma pelo usuário.

---
+++ app/Http/Middleware/LocaleMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class LocaleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user()){
+            app()->setLocale(Auth::user()->locale);
+        }elseif(Session::has('locale')){
+            $locale = Session::get('locale');
+            app()->setLocale($locale);
+        }else{
+            app()->setLocale('pt-br');
+        }
+
+        return $next($request);
+    }
+}
