a86782d117979691856b75c65f3cea1d524c360d
José Antônio <jfreitas@mat.unb.br>
2020-04-15 18:54:12 -0300

    Funções para definir o idioma preferido do usuário.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,39 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function setaLocale($locale)
+    {
+        if(Auth::check()){
+
+           $user = User::find(Auth::user()->id_user);
+
+           $user->update(['locale'=>$locale]);
+
+        }else{
+            
+            Session::put('locale',$locale);
+        }
+    }
+
+    public function getLangPortuguese()
+    {
+        $this->setaLocale('pt-br');
+
+        return redirect()->back();
+    }
+
+    public function getLangEnglish()
+    {
+        $this->setaLocale('en');
+
+        return redirect()->back();
+    }
+
+    public function getLangSpanish()
+    {
+        $this->setaLocale('es');
+
+        return redirect()->back();
+    }   
 }
