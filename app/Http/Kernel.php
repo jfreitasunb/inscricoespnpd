7683f1ec2e9c2eaf703fcdadc16cd3d2c6e2cc28
José Antônio <jfreitas@mat.unb.br>
2020-04-17 14:55:56 -0300

    Registrando middlewares.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'define.locale' => \App\Http\Middleware\LocaleMiddleware::class,
+        'user.role' => \App\Http\Middleware\UserRoles::class,
     ];
 
     /**
