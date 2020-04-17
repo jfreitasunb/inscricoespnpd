2e8276a2cfeacdedc7afe9a7ad9c382f0b69ebf1
José Antônio <jfreitas@mat.unb.br>
2020-04-16 21:27:36 -0300

    Ocultando sessões não utilizadas ainda.

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -12,63 +12,17 @@
       @include('sweetalert::alert')
       @include('templates.partials.alertas_erros')
       @include('templates.partials.cabecalho')
-        {{-- @if (Auth::check())
-          @candidato(Auth()->user())
-          <div class="container">
-            @include('templates.partials.candidato.menu_candidato')
-            @yield('dados_pessoais')
-            @yield('dados_academicos')
-            @yield('escolha_monitoria')
-            @yield('motivacao_documentos')
-            @yield('finaliza_inscricao')
-            @yield('status_cartas')
-            @yield('confirma_presenca')
-            @yield('envia_documentos_matricula')
-            @yield('processa_documentos_matricula')
-          </div>
-          @endcandidato
-          @coordenador(Auth()->user())
-          <div class="container-fluid">
-            <div class="row-fluid">
-              @include('templates.partials.coordenador.menu_coordenador')
-            </div>
-          </div>
-          @endcoordenador
-          @recomendante(Auth()->user())
-            @include('templates.partials.recomendante.menu_recomendante')
-            @yield('dados_pessoais_recomendante')
-            @yield('cartas_pendentes')
-            @yield('cartas_anteriores')
-            @yield('carta_parte_inicial')
-            @yield('carta_parte_final')
-          @endrecomendante
-          @admin(Auth()->user())
+        @if (Auth::check())
           <div class="container-fluid">
             <div class="row-fluid">
               @include('templates.partials.admin.menu_admin')
-              @impersonating_recomendante
-                @yield('dados_pessoais_recomendante')
-                @yield('cartas_pendentes')
-                @yield('carta_parte_inicial')
-                @yield('carta_parte_final')
-              @endimpersonating_recomendante
-              @impersonating_candidato
-                @yield('dados_pessoais')
-                @yield('dados_academicos')
-                @yield('escolha_monitoria')
-                @yield('motivacao_documentos')
-                @yield('finaliza_inscricao')
-                @yield('confirma_presenca')
-                @yield('status_cartas')
-              @endimpersonating_candidato
             </div>
           </div>
-          @endadmin
-        @else --}}
+        @else
           @yield('inicio')
           @yield('content')
-          {{-- @yield('ver_ficha')
-        @endif --}}
+          {{-- @yield('ver_ficha') --}}
+        @endif
       @include('templates.partials.rodape')
       <script src="https://use.fontawesome.com/96ea273a00.js"></script>
       <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
