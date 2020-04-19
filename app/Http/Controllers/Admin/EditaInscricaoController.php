afff759e85ff9082786c9e1bf2342edef29bf714
José Antônio <jfreitas@mat.unb.br>
2020-04-19 11:47:32 -0300

    Início dos controllers para editar e configurar as inscrições.

---
+++ app/Http/Controllers/Admin/EditaInscricaoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EditaInscricaoController extends Controller
+{
+    public function getEditaInscricao()
+    {
+
+    }
+
+    public function postEditaInscricao()
+    {
+        
+    }
+}
---
+++ app/Http/Controllers/Coordenador/ConfiguraInscricaoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Coordenador;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ConfiguraInscricaoController extends Controller
+{
+    public function getConfiguraInscricao()
+    {
+
+    }
+
+    public function postConfiguraInscricao()
+    {
+        
+    }
+}
