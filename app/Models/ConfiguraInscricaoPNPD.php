dd382ba6d140c1057f53411fcfa5a0fcf2fa342e
José Antônio <jfreitas@mat.unb.br>
2020-04-20 20:41:12 -0300

    Gerando o texto do cabeçalho.

--- app/Models/ConfiguraInscricaoPNPD.php
+++ app/Models/ConfiguraInscricaoPNPD.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Session;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class ConfiguraInscricaoPNPD extends Model
@@ -57,5 +59,44 @@ class ConfiguraInscricaoPNPD extends Model
             
             return $this->where('id_inscricao_pnpd', $id_inscricao_pnpd)->get()->first();
         }    
+    }
+
+    public function retorna_periodo_inscricao()
+    {
+        Session::get('locale');
+
+        if (is_null($this->retorna_edital_vigente())){
+
+            $data_inicio = '3000-01-01';
+        }else{
+            $inicio = Carbon::createFromFormat('Y-m-d', $this->retorna_edital_vigente()->inicio_inscricao);
+            
+            $fim = Carbon::createFromFormat('Y-m-d', $this->retorna_edital_vigente()->fim_inscricao);
+            
+            $data_inicio = $inicio->format('Y-m-d');
+            
+            $data_fim = $fim->format('Y-m-d');    
+        }
+
+        $data_hoje = (new Carbon())->format('Y-m-d');
+
+        if ($data_hoje >= $data_inicio && $data_hoje <= $data_fim) {
+
+            if (Session::get('locale') == 'en') {
+                
+                return $periodo_inscricao = $inicio->format('m/d/Y').trans('mensagens_gerais.to').$fim->format('m/d/Y');
+            }else{
+                
+                return $periodo_inscricao = $inicio->format('d/m/Y').trans('mensagens_gerais.to').$fim->format('d/m/Y');
+            }
+        }
+
+        if ($data_hoje < $data_inicio) {
+            return $periodo_inscricao = trans('mensagens_gerais.inscricao_nao_iniciada');
+        }
+
+        if ($data_hoje > $data_fim) {
+            return $periodo_inscricao = trans('mensagens_gerais.inscricao_encerrada');
+        }
     }
 }
