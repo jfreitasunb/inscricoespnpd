8db3bbe58e02a9bdaf2cea57db581a5bce55cac9
José Antônio <jfreitas@mat.unb.br>
2020-04-20 16:38:28 -0300

    Dependências e validação dos dados.

--- app/Http/Controllers/Coordenador/ConfiguraInscricaoController.php
+++ app/Http/Controllers/Coordenador/ConfiguraInscricaoController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Coordenador;
 
+use Carbon\Carbon;
+use App\Models\ConfiguraInscricaoPNPD;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use InscricoesPos\Notifications\NotificaNovaInscricao;
+
 
 class ConfiguraInscricaoController extends Controller
 {
@@ -12,8 +16,17 @@ class ConfiguraInscricaoController extends Controller
         return view('templates.partials.coordenador.configurar_inscricao');
     }
 
-    public function postConfiguraInscricao()
+    public function postConfiguraInscricao(Request $request)
     {
+        $this->validate($request, [
+            'inicio_inscricao' => 'required|date_format:"d/m/Y"|before:fim_inscricao|after:today',
+            'fim_inscricao' => 'required|date_format:"d/m/Y"|after:inicio_inscricao|after:today',
+            'prazo_carta' => 'required|date_format:"d/m/Y"|after:inicio_inscricao|after:today',
+            'data_homologacao' => 'required|date_format:"d/m/Y"|after:fim_inscricao|after:today',
+            'data_divulgacao_resultado' => 'required|date_format:"d/m/Y"|after:data_homologacao|after:today',
+            'necessita_recomendante' => 'required',
+        ]);
         
+        // dd($request);
     }
 }
