ce41c7bf9029a709aea7ed303cd9df551f34f47a
José Antônio <jfreitas@mat.unb.br>
2020-04-20 20:34:59 -0300

    Tratando e salvando os dados.

--- app/Http/Controllers/Admin/EditaInscricaoController.php
+++ app/Http/Controllers/Admin/EditaInscricaoController.php
@@ -15,19 +15,75 @@ class EditaInscricaoController extends Controller
         $user = Auth::user();
 
         if ($user->user_type == "admin") {
-            return view('templates.partials.admin.editar_inscricao');
+
+            $configurapnpd = new ConfiguraInscricaoPNPD();
+
+            $edital_vigente = $configurapnpd->retorna_edital_vigente();
+
+            return view('templates.partials.admin.editar_inscricao')->with(compact('edital_vigente'));
         }else{
             return redirect()->home();
         }
         
     }
 
-    public function postEditaInscricao()
+    public function postEditaInscricao(Request $request)
     {
         $user = Auth::user();
 
         if ($user->user_type == "admin") {
-            return view('templates.partials.admin.editar_inscricao');
+
+            $this->validate($request, [
+            'inicio_inscricao' => 'required|date_format:"Y-m-d"|before:fim_inscricao',
+            'fim_inscricao' => 'required|date_format:"Y-m-d"|after:inicio_inscricao',
+            'prazo_carta' => 'required|date_format:"Y-m-d"|after:inicio_inscricao',
+            'data_homologacao' => 'required|date_format:"Y-m-d"|after:fim_inscricao',
+            'data_divulgacao_resultado' => 'required|date_format:"Y-m-d"|after:data_homologacao',
+            'necessita_recomendante' => 'required',
+        ]);
+            
+        $edital_vigente = ConfiguraInscricaoPNPD::find((int)$request->id_inscricao_pnpd);
+
+        $novos_dados_edital['inicio_inscricao'] = $request->inicio_inscricao;
+        
+        $novos_dados_edital['fim_inscricao'] = $request->fim_inscricao;
+
+        $novos_dados_edital['prazo_carta'] = $request->prazo_carta;
+        
+        $novos_dados_edital['data_homologacao'] = $request->data_homologacao;
+        
+        $novos_dados_edital['data_divulgacao_resultado'] = $request->data_divulgacao_resultado;
+
+        $temp = strtolower($request->necessita_recomendante);
+
+        switch ($temp) {
+            case 'nao':
+                $necessita_recomendante = false;
+                break;
+            case 'não':
+                $necessita_recomendante = false;
+                break;
+            
+            case 'n':
+                $necessita_recomendante = false;
+                break;
+            case '0':
+                $necessita_recomendante = false;
+                break;
+                
+            default:
+                $necessita_recomendante = true;
+                break;
+        }
+
+        $novos_dados_edital['necessita_recomendante'] = $necessita_recomendante;
+
+        $edital_vigente->update($novos_dados_edital);
+
+        Alert::success('', 'Dados atualizados com sucesso!')->autoclose(3000);
+
+        return redirect()->route('editar.inscricao');
+
         }else{
             return redirect()->home();
         }
