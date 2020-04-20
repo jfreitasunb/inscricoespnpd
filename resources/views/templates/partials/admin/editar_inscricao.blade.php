faafb895346418fc4f21b07c0ed0584e94cec3e1
José Antônio <jfreitas@mat.unb.br>
2020-04-20 18:13:44 -0300

    Iniciando a tela para editar os dados da inscrição vigente.

--- resources/views/templates/partials/admin/editar_inscricao.blade.php
+++ resources/views/templates/partials/admin/editar_inscricao.blade.php
@@ -5,7 +5,76 @@
 @endsection
 
 @section('editar_inscricao')
-<h1>Teste</h1>
+<div class="row">
+   <div class="col-md-8 col-md-offset-2">
+      {!! Form::open(array('route' => 'editar.inscricao','data-parsley-validate' => '')) !!}
+        <div class="form-group row">
+            {!! Form::label('inicio_inscricao', 'Início da Inscrição', ['class' => 'col-4 col-form-label']); !!}
+            <div class="col-8">
+              <div class="input-group">
+                {!! Form::text('inicio_inscricao', null, ['class' => 'form-control', 'required' => '']) !!}
+              </div>
+            </div>
+          </div>
+          <div class="form-group row">
+            {!! Form::label('fim_inscricao', 'Fim da Inscrição', ['class' => 'col-4 col-form-label']); !!}
+            <div class="col-8">
+              <div class="input-group">
+                {!! Form::text('fim_inscricao', null, ['class' => 'form-control', 'required' => '']) !!}
+              </div>
+            </div>
+          </div>
+          <div class="form-group row">
+            {!! Form::label('prazo_carta', 'Prazo da Carta', ['class' => 'col-4 col-form-label']); !!}
+            <div class="col-8">
+              <div class="input-group">
+                {!! Form::text('prazo_carta', null, ['class' => 'form-control', 'required' => '']) !!}
+              </div>
+            </div>
+          </div>
+          <div class="form-group row">
+            {!! Form::label('data_homologacao', 'Data da Homologação', ['class' => 'col-4 col-form-label']); !!}
+            <div class="col-8">
+              <div class="input-group">
+                {!! Form::text('data_homologacao', null, ['class' => 'form-control', 'required' => '']) !!}
+              </div>
+            </div>
+          </div>
+          <div class="form-group row">
+            {!! Form::label('data_divulgacao_resultado', 'Data de Divulgação', ['class' => 'col-4 col-form-label']); !!}
+            <div class="col-8">
+              <div class="input-group">
+                {!! Form::text('data_divulgacao_resultado', null, ['class' => 'form-control', 'required' => '']) !!}
+              </div>
+            </div>
+          </div>
+          {{-- <div class="form-group row">
+            {!! Form::label('edital', 'Edital', ['class' => 'col-4 col-form-label']); !!}
+            <div class="col-8">
+                {!! Form::text('edital', null, ['class' => 'form-control', 'required' => '']) !!}
+            </div>
+          </div> --}}
+          <div class="form-group row">
+            {!! Form::label('recomendante', 'Necessita de recomendante?', ['class' => 'col-4 col-form-label']); !!}
+            <div class="col-8">
+              <div class="custom-control custom-radio custom-control-inline">
+                {!! Form::radio('necessita_recomendante', 1, true, ['id' => 'necessita_recomendante_0', 'class' => 'custom-control-input', 'required' => '']) !!}
+                <label for="necessita_recomendante_0" class="custom-control-label">Sim</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                {!! Form::radio('necessita_recomendante', 0, false, ['id' => 'necessita_recomendante_1', 'class' => 'custom-control-input', 'required' => '']) !!}
+                <label for="necessita_recomendante_1" class="custom-control-label">Não</label>
+              </div>
+            </div>
+          </div> 
+          <div class="form-group row">
+            <div class="offset-4 col-8">
+              <button name="submit" type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+          </div>
+        {!! Form::close() !!}
+    </div>
+</div>
 @endsection
 
 @section('scripts')
