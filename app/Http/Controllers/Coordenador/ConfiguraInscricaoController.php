afe1536c43dda083fd871838463fdc4277bc14f8
José Antônio <jfreitas@mat.unb.br>
2020-04-20 17:21:17 -0300

    Retorna o número do último edital.

--- app/Http/Controllers/Coordenador/ConfiguraInscricaoController.php
+++ app/Http/Controllers/Coordenador/ConfiguraInscricaoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Coordenador;
 
+use Auth;
+use Alert;
 use Carbon\Carbon;
 use App\Models\ConfiguraInscricaoPNPD;
 use App\Http\Controllers\Controller;
@@ -26,7 +28,62 @@ class ConfiguraInscricaoController extends Controller
             'data_divulgacao_resultado' => 'required|date_format:"d/m/Y"|after:data_homologacao|after:today',
             'necessita_recomendante' => 'required',
         ]);
+
+
+        $user = Auth::user();
+
+        $inicio = Carbon::createFromFormat('d/m/Y', $request->inicio_inscricao);
         
-        // dd($request);
+        $fim = Carbon::createFromFormat('d/m/Y', $request->fim_inscricao);
+        
+        $prazo = Carbon::createFromFormat('d/m/Y', $request->prazo_carta);
+        
+        $homologacao = Carbon::createFromFormat('d/m/Y', $request->data_homologacao);
+        
+        $divulgacao_resultado = Carbon::createFromFormat('d/m/Y', $request->data_divulgacao_resultado);
+
+        $necessita_recomendante = $request->necessita_recomendante;
+
+        $data_inicio = $inicio->format('Y-m-d');
+        
+        $data_fim = $fim->format('Y-m-d');
+        
+        $prazo_carta = $prazo->format('Y-m-d');
+        
+        $data_homologacao = $homologacao->format('Y-m-d');
+        
+        $data_divulgacao_resultado = $divulgacao_resultado->format('Y-m-d');
+
+        $configura_nova_inscricao_pnpd = new ConfiguraInscricaoPNPD();
+
+        $pode_configurar = $configura_nova_inscricao_pnpd->permite_configurar_inscricao($data_inicio, $data_fim);
+
+        $ano = explode("-", $data_inicio)[0];
+
+        $numero_ultimo_edital = $configura_nova_inscricao_pnpd->ultimo_edital($ano);
+
+        if ($pode_configurar) {
+            
+            $configura_nova_inscricao_pnpd->inicio_inscricao = $data_inicio;
+            
+            $configura_nova_inscricao_pnpd->fim_inscricao = $data_fim;
+            
+            $configura_nova_inscricao_pnpd->prazo_carta = $prazo_carta;
+            
+            $configura_nova_inscricao_pnpd->data_homologacao = $data_homologacao;
+            
+            $configura_nova_inscricao_pnpd->data_divulgacao_resultado = $data_divulgacao_resultado;
+            
+            $configura_nova_inscricao_pnpd->edital = ($numero_ultimo_edital+1)."-".$ano;
+            
+            $configura_nova_inscricao_pnpd->id_coordenador = $user->usuario_id;
+
+            $configura_nova_inscricao_pnpd->necessita_recomendante = $necessita_recomendante;
+
+            $configura_nova_inscricao_pnpd->save();
+        }else{
+            Alert::error('Já existe uma inscrição ativa!', 'Não é possível configurar uma nova!');
+            return redirect()->back();
+        }
     }
 }
