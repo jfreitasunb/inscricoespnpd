50dc6ec0a3d89b756c1b5cec8f88ce4a27754ae7
José Antônio <jfreitas@mat.unb.br>
2020-04-20 15:17:04 -0300

    Pegar as datas da forma correta.

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -1,8 +1,32 @@
 $(function () {
-   $('#incio_inscricao').datepicker({
-    format: "dd/mm/yyyy",
-    startView: 2,
-    language: "pt-BR",
-    autoclose: true
-});
+   $('#inicio_inscricao').datepicker({
+        format: "dd/mm/yyyy",
+        startView: 2,
+        language: "pt-BR",
+        autoclose: true
+    });
+   $('#fim_inscricao').datepicker({
+        format: "dd/mm/yyyy",
+        startView: 2,
+        language: "pt-BR",
+        autoclose: true
+    });
+   $('#prazo_carta').datepicker({
+        format: "dd/mm/yyyy",
+        startView: 2,
+        language: "pt-BR",
+        autoclose: true
+    });
+   $('#data_homologacao').datepicker({
+        format: "dd/mm/yyyy",
+        startView: 2,
+        language: "pt-BR",
+        autoclose: true
+    });
+   $('#data_divulgacao_resultado').datepicker({
+        format: "dd/mm/yyyy",
+        startView: 2,
+        language: "pt-BR",
+        autoclose: true
+    });
 });
