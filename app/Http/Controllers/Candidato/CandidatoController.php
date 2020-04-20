9cda01f18c3ea365d4630bba196e6b30cb1f7d36
José Antônio <jfreitas@mat.unb.br>
2020-04-20 20:42:28 -0300

    Inicialização do controller para gerenciar os dados do candidato.

---
+++ app/Http/Controllers/Candidato/CandidatoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Candidato;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CandidatoController extends Controller
+{
+    //
+}
