596f2f4920ce9a3daedbead00948c4d2f1fd6623
José Antônio <jfreitas@mat.unb.br>
2020-04-22 18:17:28 -0300

    Tradução da tela de inscrição.

---
+++ resources/lang/en/tela_inscricao.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+*Tradução dos textos para Português
+ */
+
+
+return [
+
+    'menu_principal' => 'Application Form',
+    'nome' => 'Name',
+    'cpf' => 'CPF',
+    'instituicao' => 'Institution where the academic degree was achieved',
+    'ano_doutorado' => 'Year',
+    'colobaradores' => 'Program members you can collaborate with',
+    'nome_recomendante' => 'Name',
+    'email_recomendante' => 'Email',
+    'curriculo' => 'Curriculum',
+    'escolha' => 'Choose file',
+    'projeto' => 'Project',
+    'enviar' => 'Submit',
+];
---
+++ resources/lang/pt-br/tela_inscricao.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+*Tradução dos textos para Português
+ */
+
+
+return [
+
+    'menu_principal' => 'Formulário de Inscrição',
+    'nome' => 'Nome Completo',
+    'cpf' => 'CPF',
+    'instituicao' => 'Instituição de obtenção do doutorado',
+    'ano_doutorado' => 'Ano',
+    'colobaradores' => 'Membros do Programa com quem pode colaborar',
+    'nome_recomendante' => 'Nome do Recomendante ',
+    'email_recomendante' => 'E-mail do Recomendante',
+    'curriculo' => 'Currículo',
+    'escolha' => 'Escolher',
+    'projeto' => 'Projeto',
+    'enviar' => 'Enviar',
+];
