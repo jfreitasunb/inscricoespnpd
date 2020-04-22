cc73f419eab83cf882fb6c998ab259bb9340a131
José Antônio <jfreitas@mat.unb.br>
2020-04-22 18:16:20 -0300

    Exibindo o formulário de inscrição de for permitido.

--- app/Http/Controllers/Candidato/CandidatoController.php
+++ app/Http/Controllers/Candidato/CandidatoController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers\Candidato;
 
+use Auth;
+use Session;
+use App\Models\ConfiguraInscricaoPNPD;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CandidatoController extends Controller
 {
-    //
+    public function getMenu()
+    {
+        Session::get('locale');
+        
+        // $user = $this->SetUser();
+        
+        // $id_user = $user->usuario_id;
+
+        $configura_inscricao = new ConfiguraInscricaoPNPD();
+
+        $libera_formulario = $configura_inscricao->autoriza_inscricao();
+
+        if ($libera_formulario) {
+            return view('templates.partials.candidato.formulario_inscricao');
+        }else{
+            return view('/');
+        }
+    }
 }
