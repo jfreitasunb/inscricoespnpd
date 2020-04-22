27d793356d745aa6bd14d0ccb1af6793e1f07c75
José Antônio <jfreitas@mat.unb.br>
2020-04-22 15:40:24 -0300

    Correção no nome da tabela.

--- database/migrations/2020_04_22_151742_create_dados_inscricao_table.php
+++ database/migrations/2020_04_22_151742_create_dados_inscricao_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAppModelsDadosInscricaosTable extends Migration
+class CreateDadosInscricaoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAppModelsDadosInscricaosTable extends Migration
      */
     public function up()
     {
-        Schema::create('app_models_dados_inscricaos', function (Blueprint $table) {
+        Schema::create('dados_inscricao', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateAppModelsDadosInscricaosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('app_models_dados_inscricaos');
+        Schema::dropIfExists('dados_inscricao');
     }
 }
