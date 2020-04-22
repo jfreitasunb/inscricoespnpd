f3e0f0b8c135b7281c99983c02a944e696912c05
José Antônio <jfreitas@mat.unb.br>
2020-04-22 15:45:39 -0300

    Tabela de finalização.

--- app/Models/FinalizaInscricao.php
+++ app/Models/FinalizaInscricao.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class FinalizaInscricao extends Model
 {
-    //
+    protected $primaryKey = 'id_candidato';
+
+    protected $table = 'finaliza_inscricao';
+
+    public function retorna_inscricao_finalizada($id_candidato, $id_inscricao_pnpd)
+    {
+        $finalizou_inscricao = $this->select('inscricao_finalizada')->where("id_candidato", $id_candidato)->where("id_inscricao_pnpd", $id_inscricao_pnpd)->get();
+
+        if (count($finalizou_inscricao)>0 and $finalizou_inscricao[0]['finalizada']) {
+            return TRUE;
+        }else{
+            return FALSE;
+        }
+    }
 }
