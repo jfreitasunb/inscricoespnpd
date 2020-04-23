dd00cab9fecae418c0ae91266f8e96fb36f31e2b
José Antônio <jfreitas@mat.unb.br>
2020-04-23 16:01:32 -0300

    Passando o número de cartas para a view.

--- app/Http/Controllers/Candidato/CandidatoController.php
+++ app/Http/Controllers/Candidato/CandidatoController.php
@@ -24,6 +24,8 @@ class CandidatoController extends Controller
 
         $id_inscricao_pnpd = $edital->id_inscricao_pnpd;
 
+        $numero_cartas = $edital->numero_cartas;
+
         $libera_formulario = $configura_inscricao->autoriza_inscricao();
 
         if (!$libera_formulario) {
@@ -31,6 +33,6 @@ class CandidatoController extends Controller
             return view('/');
         }
 
-        return view('templates.partials.candidato.formulario_inscricao')->with(compact('id_inscricao_pnpd'));
+        return view('templates.partials.candidato.formulario_inscricao')->with(compact('id_inscricao_pnpd', 'numero_cartas'));
     }
 }
