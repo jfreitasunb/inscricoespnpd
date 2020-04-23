7f024a6f7b7f265dd5316876756b84e37ed937e3
José Antônio <jfreitas@mat.unb.br>
2020-04-23 17:00:47 -0300

    Recuperando os nomes dos colaboradores.

--- app/Http/Controllers/Candidato/CandidatoController.php
+++ app/Http/Controllers/Candidato/CandidatoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Candidato;
 
 use Auth;
 use Session;
+use DB;
+use Carbon\Carbon;
 use App\Models\ConfiguraInscricaoPNPD;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -26,8 +28,6 @@ class CandidatoController extends Controller
 
         $numero_cartas = $edital->numero_cartas;
 
-        $numero_cartas = 2;
-
         $libera_formulario = $configura_inscricao->autoriza_inscricao();
 
         if (!$libera_formulario) {
@@ -35,6 +35,16 @@ class CandidatoController extends Controller
             return view('/');
         }
 
-        return view('templates.partials.candidato.formulario_inscricao')->with(compact('id_inscricao_pnpd', 'numero_cartas'));
+        $data_hoje = (new Carbon())->format('Y-m-d');
+
+        $cod_users_ativos = DB::connection('pgsql2')->table('orientateur')->where('date_fin', '>', $data_hoje)->get();
+
+        $nomes_colaboradores = [];
+
+        foreach ($cod_users_ativos as $coduser) {
+            $nomes_colaboradores[] = DB::connection('pgsql2')->select("select name||' '||firstname as nome from users us where us.coduser='".$coduser->coduser."'")[0]->nome;
+        }
+
+        return view('templates.partials.candidato.formulario_inscricao')->with(compact('id_inscricao_pnpd', 'numero_cartas', 'nomes_colaboradores'));
     }
 }
