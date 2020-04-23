d41c3f92857e715f89610813f7dfb84cdd1b37d8
José Antônio <jfreitas@mat.unb.br>
2020-04-23 18:58:15 -0300

    Ajustes na aparência do formulário.

--- resources/views/templates/partials/candidato/formulario_inscricao.blade.php
+++ resources/views/templates/partials/candidato/formulario_inscricao.blade.php
@@ -8,42 +8,38 @@
   <div class="container h-100">
     <div class="row h-100 justify-content-center align-items-center">
       <legend class="text-center">{{ trans('tela_inscricao.menu_principal') }}</legend>
-      <div class="col-10 col-md-8 col-lg-6">
+      <div class="col-10 col-md-10 col-lg-10">
         {!! Form::open(array('route' => 'candidato.inscricao','data-parsley-validate' => '' ,'enctype' => 'multipart/form-data')) !!}
           {{ Form::hidden('id_inscricao_pnpd', $id_inscricao_pnpd) }}
-          <div class="form-group">
-            {!! Form::text('nome', null, ['placeholder'=> trans('tela_inscricao.nome'),  'class' => 'form-control', 'required' => '']) !!}
-          </div>
-          <div class="form-group">
-            {!! Form::text('cpf', null, ['placeholder'=> trans('tela_inscricao.cpf'),  'class' => 'form-control', 'required' => '']) !!}
-          </div>
-          <div class="form-row">
-            <div class="form-group col-md-10">
-              <label for="instituicao">{{ trans('tela_inscricao.instituicao') }}</label>
-              {!! Form::text('instituicao', null, ['placeholder'=> trans('tela_inscricao.instituicao'),  'class' => 'form-control', 'required' => '']) !!}
+          <fieldset class="scheduler-border">
+            <legend class="scheduler-border">{{ trans('tela_inscricao.dados_pessoais') }}</legend>
+            <div class="form-group">
+              {!! Form::text('nome', null, ['placeholder'=> trans('tela_inscricao.nome'),  'class' => 'form-control', 'required' => '']) !!}
             </div>
-            <div class="form-group col-md-2">
-              <label for="ano_doutorado">{{ trans('tela_inscricao.ano_doutorado') }}</label>
-              {!! Form::text('ano_doutorado', null, ['class' => 'form-control', 'required' => '']) !!}
+            <div class="form-group">
+              {!! Form::text('cpf', null, ['placeholder'=> trans('tela_inscricao.cpf'),  'class' => 'form-control', 'required' => '']) !!}
             </div>
-          </div>
-          <div class="form-group">
-            <label>{{ trans('tela_inscricao.colobaradores') }}</label>
-            <div>
-              <div class="custom-control custom-checkbox custom-control-inline">
-                <input name="colaboradores" id="checkbox_0" type="checkbox" class="custom-control-input" value="rabbit">
-                <label for="colaboradores" class="custom-control-label">Rabbit</label>
+            <div class="form-row">
+              <div class="form-group col-md-10">
+                <label for="instituicao">{{ trans('tela_inscricao.instituicao') }}</label>
+                {!! Form::text('instituicao', null, ['placeholder'=> trans('tela_inscricao.instituicao'),  'class' => 'form-control', 'required' => '']) !!}
               </div>
-              <div class="custom-control custom-checkbox custom-control-inline">
-                <input name="checkbox" id="checkbox_1" type="checkbox" class="custom-control-input" value="duck">
-                <label for="checkbox_1" class="custom-control-label">Duck</label>
-              </div>
-              <div class="custom-control custom-checkbox custom-control-inline">
-                <input name="checkbox" id="checkbox_2" type="checkbox" class="custom-control-input" value="fish">
-                <label for="checkbox_2" class="custom-control-label">Fish</label>
+              <div class="form-group col-md-2">
+                <label for="ano_doutorado">{{ trans('tela_inscricao.ano_doutorado') }}</label>
+                {!! Form::text('ano_doutorado', null, ['class' => 'form-control', 'required' => '']) !!}
               </div>
             </div>
+        </fieldset>
+          <div class="form-group">
+            <fieldset class="scheduler-border">
+              <legend class="scheduler-border">{{ trans('tela_inscricao.colaboradores') }}</legend>
+              <p><b>{{ trans('tela_inscricao.separa_nomes') }}</b></p>
+              <p>{{ trans('tela_inscricao.lista_colaboradores') }} <a href="http://www.mat.unb.br/pagina/pesquisa-projetos" target="_blank">http://www.mat.unb.br/pagina/pesquisa-projetos</a></p>
+                {!! Form::textarea('colaboradores', null, ['class' => 'form-control', 'required' => '']) !!}
+            </fieldset>
           </div>
+          <fieldset class="scheduler-border">
+            <legend class="scheduler-border">{{ trans('tela_inscricao.recomendantes_1').$numero_cartas.trans('tela_inscricao.recomendantes_2') }}</legend>
           @for ($i = 1; $i <= $numero_cartas ; $i++)
             <div class="form-row">
               <div class="form-group col-md-6">
@@ -54,6 +50,9 @@
               </div>
             </div>
           @endfor
+        </fieldset>
+        <fieldset class="scheduler-border">
+            <legend class="scheduler-border">{{ trans('tela_inscricao.documentos') }}</legend>
           <div class="form-row">
             <div class="form-group col-md-6">
               <label for="">{{ trans('tela_inscricao.curriculo') }}</label>
@@ -70,6 +69,7 @@
               </div>
             </div>
           </div>
+        </fieldset>
           <div class="form-group row">
             <div class="offset-4 col-8">
               <button name="submit" type="submit" class="btn btn-primary">{{ trans('tela_inscricao.enviar') }}</button>
