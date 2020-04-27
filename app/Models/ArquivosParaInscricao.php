115b1c312bb90c67eb18257efc1778b467be009c
José Antônio <jfreitas@mat.unb.br>
2020-04-27 16:46:01 -0300

    Dependência e funções para atualizar e retornar nome de arquivo.

--- app/Models/ArquivosParaInscricao.php
+++ app/Models/ArquivosParaInscricao.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class ArquivosParaInscricao extends Model
@@ -21,4 +22,18 @@ class ArquivosParaInscricao extends Model
         'nome_arquivo',
         'tipo_arquivo',
     ];
+
+    public function retorna_arquivo_edital_atual($id_candidato, $id_inscricao_pnpd, $tipo_arquivo)
+    {
+        return $this->select('nome_arquivo')->where('id_candidato', $id_candidato)->where('id_inscricao_pnpd', $id_inscricao_pnpd)->where('tipo_arquivo', $tipo_arquivo)->orderBy('created_at','desc')->first();
+    }
+
+    public function atualiza_arquivos_enviados($id_candidato, $id_inscricao_pnpd, $tipo_arquivo)
+    {
+        DB::table('arquivos_para_inscricao')
+            ->where('id_candidato', $id_candidato)
+            ->where('id_inscricao_pnpd', $id_inscricao_pnpd)
+            ->where('tipo_arquivo', $tipo_arquivo)
+            ->update(['updated_at' => date('Y-m-d H:i:s')]);
+    }
 }
