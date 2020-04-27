f66ad816564247fa2fe5ce121f1674d680866284
José Antônio <jfreitas@mat.unb.br>
2020-04-27 16:24:27 -0300

    Início da model.

--- app/Models/ArquivosParaInscricao.php
+++ app/Models/ArquivosParaInscricao.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArquivosParaInscricao extends Model
 {
-    //
+    protected $table = 'arquivos_para_inscricao';
+
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_candidato',
+        'id_inscricao_pnpd',
+        'nome_arquivo',
+        'tipo_arquivo',
+    ];
 }
