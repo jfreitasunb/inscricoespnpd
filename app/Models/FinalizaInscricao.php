b76bf15d0c7bf11a143f00309ec36e2b4d04ea84
José Antônio <jfreitas@mat.unb.br>
2020-04-27 17:25:56 -0300

    Retorna o status da inscrição.

--- app/Models/FinalizaInscricao.php
+++ app/Models/FinalizaInscricao.php
@@ -10,19 +10,24 @@ class FinalizaInscricao extends Model
 
     protected $table = 'finaliza_inscricao';
 
-    public function retorna_inscricao_finalizada($id_candidato, $id_inscricao_pnpd)
-    {
-        $finalizou_inscricao = $this->select('inscricao_finalizada')->where("id_candidato", $id_candidato)->where("id_inscricao_pnpd", $id_inscricao_pnpd)->get();
+    // public function retorna_inscricao_finalizada($id_candidato, $id_inscricao_pnpd)
+    // {
+    //     $finalizou_inscricao = $this->select('inscricao_finalizada')->where("id_candidato", $id_candidato)->where("id_inscricao_pnpd", $id_inscricao_pnpd)->get();
 
-        if (count($finalizou_inscricao)>0 and $finalizou_inscricao[0]['finalizada']) {
-            return TRUE;
-        }else{
-            return FALSE;
-        }
-    }
+    //     if (count($finalizou_inscricao)>0 and $finalizou_inscricao[0]['finalizada']) {
+    //         return TRUE;
+    //     }else{
+    //         return FALSE;
+    //     }
+    // }
 
     public function retorna_tabela_inicializada($id_candidato, $id_inscricao_pnpd)
     {
         return $this->select('id')->where('id_candidato', $id_candidato)->where('id_inscricao_pnpd', $id_inscricao_pnpd)->value('id');
+    }
+
+    public function retorna_inscricao_finalizada($id_candidato, $id_inscricao_pnpd)
+    {
+        return $this->select('inscricao_finalizada')->where('id_candidato', $id_candidato)->where('id_inscricao_pnpd', $id_inscricao_pnpd)->value('inscricao_finalizada');
     }
 }
