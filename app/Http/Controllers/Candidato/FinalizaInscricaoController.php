b603683282833292bcea4e57f9191af348dee3f9
José Antônio <jfreitas@mat.unb.br>
2020-04-28 16:09:36 -0300

    Não precisa validar nada.

--- app/Http/Controllers/Candidato/FinalizaInscricaoController.php
+++ app/Http/Controllers/Candidato/FinalizaInscricaoController.php
@@ -80,36 +80,6 @@ class FinalizaInscricaoController extends Controller
             return view('/');
         }
 
-        if ($necessita_recomendante) {
-            
-            $this->validate($request, [
-                'nome' => 'required',
-                'cpf' => 'required',
-                'instituicao' => 'required',
-                'ano_doutorado' => 'required',
-                'colaboradores' => 'required',
-                'nome_recomendante' => 'required|valida_recomendantes',
-                'email_recomendante' => 'required|valida_recomendantes',
-                'confirmar_email_recomendante' => 'required|same:email_recomendante',
-                'curriculo' => 'required|max:50000|mimes:pdf',
-                'projeto' => 'required|max:50000|mimes:pdf',
-            ]);
-        }else{
-            $this->validate($request, [
-                'nome' => 'required',
-                'cpf' => 'required',
-                'instituicao' => 'required',
-                'ano_doutorado' => 'required',
-                'colaboradores' => 'required',
-                'curriculo' => 'required|max:50000|mimes:pdf',
-                'projeto' => 'required|max:50000|mimes:pdf',
-            ]);
-        }
-        
-        $nomes_recomendantes = $request->nome_recomendante;
-
-        $emails_recomendantes = $request->email_recomendante;
-
         $user = Auth::user();
 
         $usuario_id = $user->usuario_id;
