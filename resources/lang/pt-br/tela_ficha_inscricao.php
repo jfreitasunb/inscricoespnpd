25827cfa4e7cfd7a6589d873e62bed4def918881
José Antônio <jfreitas@mat.unb.br>
2020-04-28 16:05:19 -0300

    Traduções.

---
+++ resources/lang/pt-br/tela_ficha_inscricao.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+*Tradução dos textos para Português
+ */
+
+
+return [
+    'instituicao' => 'Instituição de obtenção do Doutorado',
+    'ano_doutorado' => 'Ano de obtenção do Doutorado',
+    'colaboradores' => 'Colaboradores',
+    'recomendante' => 'Recomendantes Indicados:',
+    'nome' => 'Nome',
+    'email' => 'E-mail',
+    'menu_enviar' => 'Envio Definitivo',
+    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
+    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
+];
