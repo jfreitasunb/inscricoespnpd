1ea55d0d6d60fedfbeeba3af65f51bcf97563ccb
José Antônio <jfreitas@mat.unb.br>
2020-04-28 17:24:44 -0300

    Tabela para armazenar a carta e função para informar se a carta já foi enviada.

--- app/Models/CartaRecomendacao.php
+++ app/Models/CartaRecomendacao.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class CartaRecomendacao extends Model
 {
-    //
+    protected $table = 'carta_recomendacao';
+
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_recomendante',
+        'id_candidato', 
+        'id_inscricao_pnpd',
+        'recomendacao',
+    ];
+
+    public function carta_preenchida($id_recomendante, $id_candidato, $id_inscricao_pnpd)
+    {
+        $temp = $this->where('id_recomendante', $id_recomendante)->where('id_candidato', $id_candidato)->where('id_inscricao_pnpd', $id_inscricao_pnpd)->get();
+
+        if (count($temp) == 0) {
+            return False;
+        }elseif ($temp[0]->recomendacao == "") {
+            return False;
+        }else{
+            return True;
+        }
+    }
 }
