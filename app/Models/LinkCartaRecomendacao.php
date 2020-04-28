62dec71312f3816e17673757de07f07f45ec24a1
José Antônio <jfreitas@mat.unb.br>
2020-04-28 16:50:19 -0300

    Tabela e model para gerenciar o link que o recomendante irá receber.

---
+++ app/Models/LinkCartaRecomendacao.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LinkCartaRecomendacao extends Model
+{
+    protected $primaryKey = 'id';
+
+    protected $table = 'link_carta_recomendacao';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_recomendante',
+        'id_candidato',
+        'id_inscricao_pnpd',
+        'link_acesso',
+    ];
+
+    public function link_existe($id_recomendante, $id_candidato, $id_inscricao_pnpd)
+    {
+        $temp = $this->where('id_recomendante', $id_recomendante)->where('id_candidato', $id_candidato)->where('id_inscricao_pnpd', $id_inscricao_pnpd)->get();
+
+        if (count($temp) > 0) {
+            return True;
+        }
+
+        return False;
+    }
+}
