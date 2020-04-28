8c2f9665ff42ada92b80d07a6274469a123f33b3
José Antônio <jfreitas@mat.unb.br>
2020-04-28 17:29:48 -0300

    Exibindo o status das cartas.

--- app/Http/Controllers/Candidato/CandidatoController.php
+++ app/Http/Controllers/Candidato/CandidatoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Candidato;
 use Auth;
 use Session;
 use App\Models\ConfiguraInscricaoPNPD;
+use App\Models\CartaRecomendacao;
+use App\Models\FinalizaInscricao;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -24,6 +26,7 @@ class CandidatoController extends Controller
 
         $libera_formulario = $configura_inscricao->autoriza_inscricao();
 
+
         if (!$libera_formulario) {
             
             return view('/');
@@ -32,6 +35,20 @@ class CandidatoController extends Controller
         $user = Auth::user();
 
         $nome = $user->nome;
+
+        $usuario_id = $user->usuario_id;
+
+        $finaliza_inscricao = new FinalizaInscricao();
+
+        $status_inscricao = $finaliza_inscricao->retorna_inscricao_finalizada($usuario_id, $id_inscricao_pnpd);
+
+        if ($status_inscricao) {
+
+            $carta = new CartaRecomendacao();
+
+            return view('templates.partials.candidato.status_cartas')->with(compact('dados_cartas'));
+            
+        }
 
         return view('templates.partials.candidato.formulario_inscricao')->with(compact('id_inscricao_pnpd', 'numero_cartas', 'nome'));
     }
