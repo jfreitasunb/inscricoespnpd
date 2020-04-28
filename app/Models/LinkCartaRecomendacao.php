4696added32a0eae66598be87195c36fefe61b5c
José Antônio <jfreitas@mat.unb.br>
2020-04-28 17:02:52 -0300

    Recupera o link de acesso.

--- app/Models/LinkCartaRecomendacao.php
+++ app/Models/LinkCartaRecomendacao.php
@@ -32,4 +32,9 @@ class LinkCartaRecomendacao extends Model
 
         return False;
     }
+
+    public function recupera_link_acesso($id_recomendante, $id_candidato, $id_inscricao_pnpd)
+    {
+        return $this->select('link_acesso')->where('id_recomendante', $id_recomendante)->where('id_candidato', $id_candidato)->where('id_inscricao_pnpd', $id_inscricao_pnpd)->value('link_acesso');
+    }
 }
