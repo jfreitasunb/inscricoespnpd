dd2a9c15202f1d9cebee17bbfe4832a2480b1e17
José Antônio <jfreitas@mat.unb.br>
2020-04-29 18:46:49 -0300

    Controller para o Recomendante.

---
+++ app/Http/Controllers/Recomendante/RecomendanteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Recomendante;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RecomendanteController extends Controller
+{
+    public function getLink()
+    {
+        
+    }
+}
