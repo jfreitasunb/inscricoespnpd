c691a40b2dfd992018f49cc21a8c7bd5bd297b47
José Antônio <jfreitas@mat.unb.br>
2020-04-29 19:00:05 -0300

    Recuperando parâmentros da url.

--- app/Http/Controllers/Recomendante/RecomendanteController.php
+++ app/Http/Controllers/Recomendante/RecomendanteController.php
@@ -10,7 +10,10 @@ class RecomendanteController extends Controller
     public function getLink(Request $request)
     {
         $link_acesso = $request->token;
-        $id_recomendante = $request->reco;
+        
+        $id_recomendante = explode("-", $request->reco)[0];
+
+        $id_inscricao_pnpd = explode("-", $request->reco)[1];
 
         dd($id_recomendante);
     }
