d2357a5334e35fe4a51cb1c0b61f448b3a180f50
José Antônio <jfreitas@mat.unb.br>
2020-04-30 11:25:39 -0300

    Não permite envio fora do prazo.

--- app/Http/Controllers/Recomendante/RecomendanteController.php
+++ app/Http/Controllers/Recomendante/RecomendanteController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Recomendante;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\LinkCartaRecomendacao;
 
 class RecomendanteController extends Controller
-{
+{   
     public function getLink(Request $request)
     {
+        $inscricao = new ConfiguraInscricaoPNPD();
+
+        $autoriza_carta = $inscricao->autoriza_carta();
+
+        if (!$autoriza_carta) {
+
+            Alert::error('Dear professor, the deadline for sending letters is closed.');
+
+            return redirect('/');
+        }
+
         $link_acesso = $request->token;
         
         $id_recomendante = explode("-", $request->reco)[0];
