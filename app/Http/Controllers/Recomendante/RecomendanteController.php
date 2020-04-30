91561cdf0e1b2d030aa8f1b05662451ca4d4eaa9
José Antônio <jfreitas@mat.unb.br>
2020-04-30 11:48:35 -0300

    Validando se pode exibir a tela de preenchimento da carta.

--- app/Http/Controllers/Recomendante/RecomendanteController.php
+++ app/Http/Controllers/Recomendante/RecomendanteController.php
@@ -27,11 +27,52 @@ class RecomendanteController extends Controller
         }
 
         $link_acesso = $request->token;
-        
+
+        $link = new LinkCartaRecomendacao();
+
+        $dados_link = $link->retorna_dados_link($link_acesso);
+
+        if (count($dados_link)> 1 or count($dados_link) == 0) {
+            
+            return redirect('/');
+        }
+
+        $dados_link = $link->retorna_dados_link($link_acesso)[0];
+
+        $edital = $inscricao->retorna_edital_vigente();
+
+        $id_inscricao_pnpd = $edital->id_inscricao_pnpd;
+
         $id_recomendante = explode("-", $request->reco)[0];
 
-        $id_inscricao_pnpd = explode("-", $request->reco)[1];
+        $id_inscricao = explode("-", $request->reco)[1];
 
-        dd($id_recomendante);
+        if (!$edital->necessita_recomendante) {
+            
+            return redirect('/');
+        }
+
+        if ($edital->id_inscricao_pnpd != $id_inscricao) {
+            
+            return redirect('/');
+        }
+
+        if ($dados_link->id_recomendante != $id_recomendante ) {
+            
+            return redirect('/');
+        }
+
+        $dados = new DadosInscricao();
+
+        $dados_candidato = $dados->retorna_dados_inscricao($dados_link->id_candidato, $id_inscricao_pnpd);
+
+        if (strpos($dados_candidato[0]->recomendantes, $id_recomendante) == false) {
+            
+            return redirect('/');
+        }
+
+        
+
+
     }
 }
