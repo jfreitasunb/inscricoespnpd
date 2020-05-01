dec58a447bc00af2391940031b6a0e2543b52086
José Antônio <jfreitas@mat.unb.br>
2020-05-01 09:42:40 -0300

    Tradução da tela com a carta.

---
+++ resources/lang/en/tela_carta_recomendacao.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+*Tradução dos textos para Português
+ */
+
+
+return [
+    'titulo_principal' => 'Recomendation letter',
+    'carta_candidato' => 'To the candidate: ',
+    'nome_recomendante' => 'Name',
+    'instituticao_recomendante' => 'Institution',
+    'recomendacao' => 'Fill the recomendation letter below',
+    'menu_enviar' => 'Sent',
+];
