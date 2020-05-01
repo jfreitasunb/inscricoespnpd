8d84e1640e3424d063922aa6eb597683a5e92d9e
José Antônio <jfreitas@mat.unb.br>
2020-05-01 09:43:37 -0300

    Scripts não utilizados.

--- resources/views/templates/partials/recomendante/carta_recomendacao.blade.php
+++ resources/views/templates/partials/recomendante/carta_recomendacao.blade.php
@@ -42,12 +42,3 @@
     </div>
   </div>
 @endsection
-
-@section('scripts')
-  {!! Html::script( asset('bower_components/moment/min/moment.min.js') ) !!}
-  {!! Html::script( asset('bower_components/moment/locale/pt-br.js') ) !!}
-  {!! Html::script( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') ) !!}
-  {!! Html::script( asset('bower_components/moment/locale/fr.js') ) !!}
-  {!! Html::script( asset('js/datepicker.js') ) !!}
-  {!! Html::script( asset('js/parsley.min.js') ) !!}
-@endsection
