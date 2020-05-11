4e811e1e98fbaab27969113d260be21af269181d
José Antônio <jfreitas@mat.unb.br>
2020-05-11 16:58:59 -0300

    Ajustes nas colunas.

--- resources/views/templates/partials/coordenador/ficha_individual.blade.php
+++ resources/views/templates/partials/coordenador/ficha_individual.blade.php
@@ -8,8 +8,7 @@
     <table class="table table-bordered table-hover">
       <thead>
         <tr>
-          <th scope="col">{{ trans('tela_cartas_pendentes.nome_candidato') }}</th>
-          <th scope="col">{{ trans('tela_cartas_pendentes.tipo_programa') }}</th>
+          <th scope="col">Nome do Candidato</th>
           <th>Ficha de Inscrição</th>
           <th>Cartas Recebidas</th>
         </tr>
@@ -18,7 +17,6 @@
         @foreach( $inscricoes_finalizadas as $finalizada)
           <tr class="{{ $classes_linhas[$total_cartas[$finalizada['id_candidato']]] }}">
             <td><a href=" {{ route('ver.ficha.individual', ['id_inscricao_pos' => $finalizada['id_inscricao_pos'],'id_aluno' => $finalizada['id_candidato']]) }}">{{ $finalizada['nome'] }}</a></td>
-            <td><a href=" {{ route('ver.ficha.individual', ['id_inscricao_pos' => $finalizada['id_inscricao_pos'],'id_aluno' => $finalizada['id_candidato']]) }}">{{ $finalizada['tipo_programa_pos_ptbr'] }}</a></td>
             <td>@if($id_aluno_pdf == $finalizada['id_candidato']) <a target="_blank" href="{{asset($nome_pdf)}}" > Ficha de Inscrição </a> @endif</td>
             <td>{{ $total_cartas[$finalizada['id_candidato']] }}</td>
           </tr>
