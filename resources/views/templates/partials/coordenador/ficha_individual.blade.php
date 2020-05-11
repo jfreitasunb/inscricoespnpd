2bae9b03df9c36f12b5faea95300723624ca94d2
José Antônio <jfreitas@mat.unb.br>
2020-05-11 16:42:20 -0300

    Coisas não utilizadas.

--- resources/views/templates/partials/coordenador/ficha_individual.blade.php
+++ resources/views/templates/partials/coordenador/ficha_individual.blade.php
@@ -1,16 +1,9 @@
 @extends('templates.default')
 
-@section('stylesheets')
-  {!! Html::style( asset('css/parsley.css') ) !!}
-  {!! Html::style( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') ) !!}
-  {!! Html::style( asset('bower_components/moment/locale/fr.js') ) !!}
-@endsection
-
 @section('ficha_individual')
 
 <fieldset class="scheduler-border">
   <legend class="scheduler-border">Fichas de Inscrição Individuais</legend>
-
   <div class="table-responsive">
     <table class="table table-bordered table-hover">
       <thead>
@@ -35,13 +28,4 @@
   </div>
 </fieldset>
 
-@endsection
-
-@section('scripts')
-  {!! Html::script( asset('bower_components/moment/min/moment.min.js') ) !!}
-  {!! Html::script( asset('bower_components/moment/locale/pt-br.js') ) !!}
-  {!! Html::script( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') ) !!}
-  {!! Html::script( asset('bower_components/moment/locale/fr.js') ) !!}
-  {!! Html::script( asset('js/datepicker.js') ) !!}
-  {!! Html::script( asset('js/parsley.min.js') ) !!}
 @endsection
