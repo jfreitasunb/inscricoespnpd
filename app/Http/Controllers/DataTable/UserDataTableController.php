67bff33bd98ba470c78107107e0cd06081104d80
José Antônio <jfreitas@mat.unb.br>
2020-05-12 14:40:41 -0300

    Correção no nome de campo e remoção de campo inexistente.

--- app/Http/Controllers/DataTable/UserDataTableController.php
+++ app/Http/Controllers/DataTable/UserDataTableController.php
@@ -19,30 +19,29 @@ class UserDataTableController extends DataTableController
     public function getDisplayableColumns()
     {
         return [
-            'id_user', 'nome', 'email', 'locale', 'user_type', 'ativo'
+            'usuario_id', 'nome', 'email', 'locale', 'user_type'
         ];
     }
 
     public function getUpdatableColumns()
     {
         return [
-            'nome', 'email', 'locale', 'user_type', 'ativo'
+            'nome', 'email', 'locale', 'user_type'
         ];
     }
 
     public function getCustomColumnNanes()
     {
         return [
-            'id_user' => 'Identificador',
+            'usuario_id' => 'Identificador',
             'nome' => 'Nome',
             'email' => 'E-mail',
             'locale' => 'Idioma',
             'user_type' => 'Tipo de Usuário',
-            'ativo' => 'Ativo',
         ];
     }
 
-    public function update($id_user, Request $request)
+    public function update($usuario_id, Request $request)
     {
         $this->validate($request, [
             'nome' => 'required|max:255',
@@ -53,9 +52,8 @@ class UserDataTableController extends DataTableController
             'user_type' => [
                     'required', Rule::in(['coordenador', 'candidato', 'recomendante'])
                         ],
-            'ativo' => 'required',
         ]);
 
-        $this->builder->find($id_user)->update($request->only($this->getUpdatableColumns()));
+        $this->builder->find($usuario_id)->update($request->only($this->getUpdatableColumns()));
     }
 }
