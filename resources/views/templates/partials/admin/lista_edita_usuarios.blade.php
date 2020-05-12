98ca54fa5e70360c65c9784df7a84b50e0c2c1ba
José Antônio <jfreitas@mat.unb.br>
2020-05-12 14:54:24 -0300

    Coisas não utilizadas.

--- resources/views/templates/partials/admin/lista_edita_usuarios.blade.php
+++ resources/views/templates/partials/admin/lista_edita_usuarios.blade.php
@@ -1,11 +1,5 @@
 @extends('templates.default')
 
-@section('stylesheets')
-  {!! Html::style( asset('css/parsley.css') ) !!}
-  {!! Html::style( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') ) !!}
-  {!! Html::style( asset('bower_components/moment/locale/fr.js') ) !!}
-@endsection
-
 @section('lista_edita_usuarios')
 
 <div id="app">
@@ -16,11 +10,4 @@
   </div>
 </div>
 
-@endsection
-
-@section('scripts')
-  {!! Html::script( asset('bower_components/moment/min/moment.min.js') ) !!}
-  {!! Html::script( asset('bower_components/moment/locale/pt-br.js') ) !!}
-  {!! Html::script( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') ) !!}
-  {!! Html::script( asset('bower_components/moment/locale/fr.js') ) !!}
 @endsection
